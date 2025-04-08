7d7e1bc9592261544cf11d87ea373371538c0e6e
hitchlin <hitchlin1109@gmail.comm>
2025-04-08 15:57:48 -0500

    submission

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileButton = document.querySelector(".profile__edit-button");
-const editProtileCloseButton = editProfileModal.querySelector(
+const editProfileCloseButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
 
@@ -8,12 +8,12 @@ editProfileButton.addEventListener("click", function () {
   editProfileModal.classList.add("modal__is-opened");
 });
 
-editProtileCloseButton.addEventListener("click", function () {
+editProfileCloseButton.addEventListener("click", function () {
   editProfileModal.classList.remove("modal__is-opened");
 });
 
 const newPostModal = document.querySelector("#new-post-modal");
-const newPostButton = document.querySelector(".modal-submit-button");
+const newPostButton = document.querySelector(".profile__add-button");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
 
 newPostButton.addEventListener("click", function () {
